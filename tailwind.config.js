2b089dd4c0edc338c0dd5f24bd96e32df0b8038d
Hamkawastaken <hamkarifai49@gmail.com>
2024-09-24 15:53:52 +0700

    fourth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
         koulen: "Koulen",
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 };
